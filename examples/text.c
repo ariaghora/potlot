8bd553889652d4b9e504fe3114cc9217a063e22a
Aria Ghora Prabono <hello@ghora.net>
2024-08-30 00:43:45 +0900

    update example

--- examples/text.c
+++ examples/text.c
@@ -8,7 +8,7 @@ int main(void) {
 
   plt_image *img = plt_init(img_width, img_height);
 
-  plt_text_draw(img, "hello world, this is...", 10, 10, 0xff00ff);
+  plt_text_draw(img, "Hello world, this is...", 10, 10, 0xff00ff);
 
   const char *large_text = "potlot!";
   int text_width_px = plt_text_measure(large_text, 2);
@@ -16,8 +16,6 @@ int main(void) {
                        img_width/2 - text_width_px/2,
                        img_height/2 - 5,
                        0xffff00, 2);
-
-  printf("%ld\n", strlen(large_text));
 
   plt_save_ppm(img, "text.ppm");
   plt_free(img);
