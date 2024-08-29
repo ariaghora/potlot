6276b32a55de1396ab57cec932b7f34e0e2885a0
Aria Ghora Prabono <hello@ghora.net>
2024-08-30 00:23:13 +0900

    update API, fix line drawing

---
+++ examples/text.c
@@ -0,0 +1,24 @@
+// clang-format off
+
+#define POTLOT_IMPLEMENTATION
+#include "../potlot.h"
+
+int main(void) {
+  int img_width = 300, img_height = 150;
+
+  plt_image *img = plt_init(img_width, img_height);
+
+  plt_text_draw(img, "hello world, this is...", 10, 10, 0xff00ff);
+
+  const char *large_text = "potlot!";
+  int text_width_px = plt_text_measure(large_text, 2);
+  plt_text_draw_scaled(img, large_text,
+                       img_width/2 - text_width_px/2,
+                       img_height/2 - 5,
+                       0xffff00, 2);
+
+  printf("%ld\n", strlen(large_text));
+
+  plt_save_ppm(img, "text.ppm");
+  plt_free(img);
+}
--- potlot.h
+++ potlot.h
@@ -9,10 +9,10 @@
 #define GLYPH_SIZE     5
 #define LETTER_SPACING 2
 
-typedef struct plt_plot {
+typedef struct plt_image {
   int  width, height;
   int *pixels;
-} plt_plot;
+} plt_image;
 
 typedef enum {
   PT_ERRCODE_OK, PT_ERRCODE_IO_ERROR,
@@ -83,17 +83,18 @@ static char plt_glyphs[128][GLYPH_SIZE*GLYPH_SIZE] = {
 extern "C" {
 #endif
 
-PTDEF plt_plot  *plt_init(int width, int height);
-PTDEF void       plt_free(plt_plot *plot);
-PTDEF pt_errcode plt_save_ppm(plt_plot *plot, const char *filename);
+PTDEF plt_image  *plt_init(int width, int height);
+PTDEF void        plt_free(plt_image *im);
+PTDEF pt_errcode  plt_save_ppm(plt_image *im, const char *filename);
 
 /*
  * Drawing API
  */
-PTDEF void plt_pixel_put(plt_plot *plot, int x, int y, int color, float alpha);
-PTDEF void plt_draw_line(plt_plot *plot, int x0, int y0, int x1, int y1, int color, int thickness);
-PTDEF void plt_draw_text(plt_plot *plot, const char *text, int x, int y, int color);
-PTDEF void plt_draw_text_scaled( plt_plot *plot, const char *text, int x, int y, int color, int scale);
+PTDEF void plt_pixel_put(plt_image *im, int x, int y, int color, float alpha);
+PTDEF void plt_draw_line(plt_image *im, int x0, int y0, int x1, int y1, int color, int thickness);
+PTDEF void plt_text_draw(plt_image *im, const char *text, int x, int y, int color);
+PTDEF void plt_text_draw_scaled(plt_image *im, const char *text, int x, int y, int color, int scale);
+PTDEF int  plt_text_measure(const char *text, int scale);
 
 #ifdef  __cplusplus
 }
@@ -102,50 +103,60 @@ PTDEF void plt_draw_text_scaled( plt_plot *plot, const char *text, int x, int y,
 #ifdef  POTLOT_IMPLEMENTATION
 #include <math.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 
 #define PLT_MALLOC(s) malloc(s)
 #define PLT_FREE(s)   free(s)
-#define PLT_RGB(r,g,b) (((r&0xff) << 16) | ((g&0xff) << 8) | (b & 0xff))
-#define XORSWAP(a, b) do { a=a^b; b=a^b; a=a^b; } while(0);
 
-PTDEF plt_plot *plt_init(int width, int height) {
-  plt_plot *plt = (plt_plot*) PLT_MALLOC(sizeof(*plt));
+#define plt__xorswap(a, b) do { a=a^b; b=a^b; a=a^b; } while(0);
+#define plt__rgb(r,g,b)    (((r&0xff) << 16) | ((g&0xff) << 8) | (b & 0xff))
+#define plt__r_i32(px)     ((px >> 16) & 0xff)
+#define plt__g_i32(px)     ((px >> 8) & 0xff)
+#define plt__b_i32(px)     (px & 0xff)
+#define plt__r_f32(px)     (plt__r_i32(px) / 255f)
+#define plt__g_f32(px)     (plt__g_i32(px) / 255f)
+#define plt__b_f32(px)     (plt__b_i32(px) / 255f)
+
+PTDEF plt_image *plt_init(int width, int height) {
+  plt_image *plt = (plt_image*) PLT_MALLOC(sizeof(*plt));
   plt->pixels   = (int*) PLT_MALLOC(sizeof(int) * width * height);
   plt->width    = width; plt->height=height;
   for (int i = 0; i < width*height; ++i) plt->pixels[i]=0;
   return plt;
 }
 
-PTDEF void plt_free(plt_plot *plot) {
-  PLT_FREE(plot->pixels);
-  PLT_FREE(plot);
+PTDEF void plt_free(plt_image *im) {
+  PLT_FREE(im->pixels);
+  PLT_FREE(im);
 }
 
-PTDEF void plt_pixel_put(plt_plot *plot, int x, int y, int color, float alpha) {
-  int px   = plot->pixels[y*plot->width + x];
-  int r    = (color >> 16) & 0xff;
-  int g    = (color >> 8) & 0xff;
-  int b    =  color & 0xff;
-  int bg_r = (px >> 16) & 0xff;
-  int bg_g = (px >> 8) & 0xff;
-  int bg_b =  px & 0xff;
-  plot->pixels[y*plot->width + x] = PLT_RGB(
+PTDEF void plt_pixel_put(plt_image *im, int x, int y, int color, float alpha) {
+  /* This will put pixel on buffer with alpha blending strategy. */
+  int r    = plt__r_i32(color);
+  int g    = plt__g_i32(color);
+  int b    = plt__b_i32(color);
+  int px   = im->pixels[y*im->width + x];
+  int bg_r = plt__r_i32(px);
+  int bg_g = plt__g_i32(px);
+  int bg_b = plt__b_i32(px);
+  im->pixels[y*im->width + x] = plt__rgb(
     (int)(bg_r * (1-alpha) + r*alpha),
     (int)(bg_g * (1-alpha) + g*alpha),
     (int)(bg_b * (1-alpha) + b*alpha)
   );
 }
 
-PTDEF void plt_draw_line(plt_plot *plot, int x0, int y0, int x1, int y1, int color, int width) {
+PTDEF void plt_draw_line(plt_image *im, int x0, int y0, int x1, int y1, int color, int width) {
   if (width < 1) width = 1;
   int steep = abs(y1-y0) > abs(x1-x0);
-  if (steep) { XORSWAP(x0, y0); XORSWAP(x1, y1); }
-  if (x0 > x1) { XORSWAP(x0, x1); XORSWAP(y0, y1); }
 
-  float dx = x1 - x0;
-  float dy = y1 - y0;
-  float grad = dx > 0 ? dy/dx : 1.0;
+  if (steep)   { plt__xorswap(x0, y0); plt__xorswap(x1, y1); }
+  if (x0 > x1) { plt__xorswap(x0, x1); plt__xorswap(y0, y1); }
+
+  float dx   = x1 - x0;
+  float dy   = y1 - y0;
+  float grad = dx > 0 ? (dy/dx) : 1.0;
 
   // rotate w
   width = width * sqrt(1 + grad*grad);
@@ -160,17 +171,15 @@ PTDEF void plt_draw_line(plt_plot *plot, int x0, int y0, int x1, int y1, int col
   float rfpart = 1 - fpart;
 
   if (steep) {
-    plt_pixel_put(plot, ypxl1, xpxl1, color, rfpart*xgap);
-    for (int i = 0; i < width; ++i) {
-      plt_pixel_put(plot, ypxl1 + i, xpxl1, color, 1);
-    }
-    plt_pixel_put(plot, ypxl1 + width, xpxl1, color, fpart*xgap);
+    plt_pixel_put(im, ypxl1, xpxl1, color, rfpart*xgap);
+    for (int i = 1; i < width; ++i) 
+      plt_pixel_put(im, ypxl1 + i, xpxl1, color, 1);
+    plt_pixel_put(im, ypxl1 + width, xpxl1, color, fpart*xgap);
   } else {
-    plt_pixel_put(plot, xpxl1, ypxl1, color, rfpart*xgap);
-    for (int i = 0; i < width; ++i) {
-      plt_pixel_put(plot, xpxl1 + i, ypxl1, color, 1);
-    }
-    plt_pixel_put(plot, xpxl1 + width, ypxl1, color, fpart*xgap);
+    plt_pixel_put(im, xpxl1, ypxl1, color, rfpart*xgap);
+    for (int i = 1; i < width; ++i) 
+      plt_pixel_put(im, xpxl1, ypxl1 + i, color, 1);
+    plt_pixel_put(im, xpxl1, ypxl1 + width, color, fpart*xgap);
   }
 
   float intery = y_end + grad;
@@ -184,17 +193,15 @@ PTDEF void plt_draw_line(plt_plot *plot, int x0, int y0, int x1, int y1, int col
   rfpart = 1 - fpart;
 
   if (steep) {
-    plt_pixel_put(plot, ypxl2, xpxl2, color, rfpart*xgap);
-    for (int i = 0; i < width; ++i) {
-      plt_pixel_put(plot, ypxl2 + i, xpxl2, color, 1);
-    }
-    plt_pixel_put(plot, ypxl2 + width, xpxl2, color, fpart*xgap);
+    plt_pixel_put(im, ypxl2, xpxl2, color, rfpart*xgap);
+    for (int i = 1; i < width; ++i) 
+      plt_pixel_put(im, ypxl2 + i, xpxl2, color, 1);
+    plt_pixel_put(im, ypxl2 + width, xpxl2, color, fpart*xgap);
   } else {
-    plt_pixel_put(plot, xpxl2, ypxl2, color, rfpart*xgap);
-    for (int i = 0; i < width; ++i) {
-      plt_pixel_put(plot, xpxl2, ypxl2 + i, color, 1);
-    }
-    plt_pixel_put(plot, xpxl2, ypxl2 + width, color, fpart*xgap);
+    plt_pixel_put(im, xpxl2, ypxl2, color, rfpart*xgap);
+    for (int i = 1; i < width; ++i) 
+      plt_pixel_put(im, xpxl2, ypxl2 + i, color, 1);
+    plt_pixel_put(im, xpxl2, ypxl2 + width, color, fpart*xgap);
   }
 
   if (steep) {
@@ -203,11 +210,10 @@ PTDEF void plt_draw_line(plt_plot *plot, int x0, int y0, int x1, int y1, int col
       rfpart = 1 - fpart;
       float y = floor(intery);
 
-      plt_pixel_put(plot, y, x, color, rfpart);
-      for (int i = 1; i < width; ++i) {
-        plt_pixel_put(plot, y + i, x, color, 1);
-      }
-      plt_pixel_put(plot, y + width, x, color, fpart);
+      plt_pixel_put(im, y, x, color, rfpart);
+      for (int i = 1; i < width; ++i)
+        plt_pixel_put(im, y + i, x, color, 1);
+      plt_pixel_put(im, y + width, x, color, fpart);
 
       intery = intery + grad;
     }
@@ -217,52 +223,57 @@ PTDEF void plt_draw_line(plt_plot *plot, int x0, int y0, int x1, int y1, int col
       rfpart = 1 - fpart;
       float y = floor(intery);
 
-      plt_pixel_put(plot, x, y, color, rfpart);
-      for (int i = 1; i < width; ++i) {
-        plt_pixel_put(plot, x, y + i, color, 1);
-      }
-      plt_pixel_put(plot, x, y + width, color, fpart);
+      plt_pixel_put(im, x, y, color, rfpart);
+      for (int i = 1; i < width; ++i)
+        plt_pixel_put(im, x, y + i, color, 1);
+      plt_pixel_put(im, x, y + width, color, fpart);
 
       intery = intery + grad;
     }
   }
 }
 
-PTDEF void plt_draw_text(plt_plot *plot, const char *text, int x, int y, int color) {
-  plt_draw_text_scaled(plot, text, x, y, color, 1);
+PTDEF void plt_text_draw(plt_image *im, const char *text, int x, int y, int color) {
+  plt_text_draw_scaled(im, text, x, y, color, 1);
 }
 
-PTDEF void plt_draw_text_scaled(plt_plot *plot, const char *text, int x, int y, int color, int scale) {
-  int len = strlen(text);
+PTDEF void plt_text_draw_scaled(plt_image *im, const char *text, int x, int y, int color, int scale) {
+  int len        = strlen(text);
   for (int cidx=0; cidx<len; ++cidx) {
-    for (int crow=0; crow<GLYPH_SIZE*scale; crow+=scale) {
-      for (int ccol=0; ccol<GLYPH_SIZE*scale; ccol+=scale) {
-        int px_x = ccol+x + cidx*(GLYPH_SIZE*scale + LETTER_SPACING);
-        int px_y = crow+y;
+    for (int c_row=0; c_row<GLYPH_SIZE*scale; c_row+=scale) {
+      for (int c_col=0; c_col<GLYPH_SIZE*scale; c_col+=scale) {
+        int px_x = c_col+x + cidx*(GLYPH_SIZE*scale + LETTER_SPACING);
+        int px_y = c_row+y;
+
         int inside_canvas = (px_y>=0 && px_x>=0 &&
-                             px_y<plot->height && px_x<plot->width);
-        int has_value = plt_glyphs[text[cidx]][(crow*GLYPH_SIZE)/scale+ccol/scale]==1;
+                             px_y<im->height && px_x<im->width);
+        int has_value     = plt_glyphs[text[cidx]][(c_row*GLYPH_SIZE)/scale + (c_col/scale)]==1;
 
         if (inside_canvas && has_value) {
-          for (int rrow=0; rrow<scale; ++rrow)
-            for (int rcol=0; rcol<scale; ++rcol)
-              plt_pixel_put(plot, px_x+rcol, px_y+rrow, color, 1);
+          for (int rep_row=0; rep_row<scale; ++rep_row)
+            for (int rep_col=0; rep_col<scale; ++rep_col)
+              plt_pixel_put(im, px_x+rep_col, px_y+rep_row, color, 1);
         }
       }
     }
   }
 }
 
-PTDEF pt_errcode plt_save_ppm(plt_plot *plot, const char *filename) {
+PTDEF int plt_text_measure(const char *text, int scale){
+  if (strlen(text) == 0) return 0;
+  return (GLYPH_SIZE*scale + LETTER_SPACING)*(strlen(text)) - LETTER_SPACING;
+}
+
+PTDEF pt_errcode plt_save_ppm(plt_image *im, const char *filename) {
   FILE* fp = fopen(filename, "wb");
   if (!fp) return PT_ERRCODE_IO_ERROR;
 
   // Write PPM header
-  fprintf(fp, "P3\n%d %d\n255\n", plot->width, plot->height);
+  fprintf(fp, "P3\n%d %d\n255\n", im->width, im->height);
   // Write pixels to PPM file
-  for (int row = 0; row < plot->height; ++row) {
-    for (int col = 0; col < plot->width; ++col) {
-      int px = plot->pixels[row * plot->width + col];
+  for (int row = 0; row < im->height; ++row) {
+    for (int col = 0; col < im->width; ++col) {
+      int px = im->pixels[row * im->width + col];
       int r = (px >> 16) & 0xff;
       int g = (px >> 8) & 0xff;
       int b = px & 0xff;
